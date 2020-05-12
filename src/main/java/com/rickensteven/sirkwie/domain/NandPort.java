848b1ac5ce3736d5de27d84b503dee2cccadbad0
Steven Liebregt <s.liebregt@outlook.com>
2020-05-12 13:12:01 +0200

    added NAND port

---
+++ src/main/java/com/rickensteven/sirkwie/domain/NandPort.java
@@ -0,0 +1,29 @@
+package com.rickensteven.sirkwie.domain;
+
+import java.util.List;
+
+/**
+ * The output of a NAND port is '1' when not both inputs are '1'.
+ *
+ *   Input A | Input B | Output
+ *  ---------|---------|--------
+ *         0 |       0 |      1
+ *         0 |       1 |      1
+ *         1 |       0 |      1
+ *         1 |       1 |      0
+ */
+public class NandPort extends Port
+{
+    public NandPort(List<Node> previous)
+    {
+        super(previous);
+    }
+
+    @Override
+    public boolean calculate()
+    {
+        return previous.stream()
+                .filter(node -> node.value.getValue())
+                .count() != previous.size();
+    }
+}
---
+++ src/test/java/com/rickensteven/sirkwie/NandPortTest.java
@@ -0,0 +1,87 @@
+package com.rickensteven.sirkwie;
+
+import com.rickensteven.sirkwie.domain.Input;
+import com.rickensteven.sirkwie.domain.NandPort;
+import com.rickensteven.sirkwie.domain.Node;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NandPortTest
+{
+    /**
+     *   Input A | Input B | Output
+     *  ---------|---------|--------
+     *         0 |       0 |      1
+     */
+    @Test
+    public void shouldReturnOnA()
+    {
+        List<Node> inputs = new ArrayList<Node>() {{
+            add(new Input(false));
+            add(new Input(false));
+        }};
+
+        NandPort nandPort = new NandPort(inputs);
+
+        assertTrue(nandPort.calculate());
+    }
+
+    /**
+     *   Input A | Input B | Output
+     *  ---------|---------|--------
+     *         0 |       1 |      1
+     */
+    @Test
+    public void shouldReturnOnB()
+    {
+        List<Node> inputs = new ArrayList<Node>() {{
+            add(new Input(false));
+            add(new Input(true));
+        }};
+
+        NandPort nandPort = new NandPort(inputs);
+
+        assertTrue(nandPort.calculate());
+    }
+
+    /**
+     *   Input A | Input B | Output
+     *  ---------|---------|--------
+     *         1 |       0 |      1
+     */
+    @Test
+    public void shouldReturnOnC()
+    {
+        List<Node> inputs = new ArrayList<Node>() {{
+            add(new Input(true));
+            add(new Input(false));
+        }};
+
+        NandPort nandPort = new NandPort(inputs);
+
+        assertTrue(nandPort.calculate());
+    }
+
+    /**
+     *   Input A | Input B | Output
+     *  ---------|---------|--------
+     *         1 |       1 |      0
+     */
+    @Test
+    public void shouldReturnOff()
+    {
+        List<Node> inputs = new ArrayList<Node>() {{
+            add(new Input(true));
+            add(new Input(true));
+        }};
+
+        NandPort nandPort = new NandPort(inputs);
+
+        assertFalse(nandPort.calculate());
+    }
+}
