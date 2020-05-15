9a318216150a2c4c2e64d6d4fa1b9dece941659b
Steven Liebregt <s.liebregt@outlook.com>
2020-05-15 09:21:16 +0200

    update circuit

--- core/src/main/java/com/rickensteven/sirkwie/core/domain/Circuit.java
+++ core/src/main/java/com/rickensteven/sirkwie/core/domain/Circuit.java
@@ -8,6 +8,48 @@ public class Circuit
     private Set<Node> inputs;
     private Set<Node> probes;
 
+    public Circuit(Set<Node> nodes, Set<Node> inputs, Set<Node> probes)
+    {
+        this.nodes = nodes;
+        this.inputs = inputs;
+        this.probes = probes;
+    }
+
+    /**
+     * Adds a node to the list of nodes.
+     * @param node The node to add.
+     * @return The current circuit.
+     */
+    public Circuit addNode(Node node)
+    {
+        this.nodes.add(node);
+        return this;
+    }
+
+    /**
+     * Adds a node to the list of inputs, and to the list of nodes.
+     * @see #addNode
+     * @param node The node to add.
+     * @return The current circuit.
+     */
+    public Circuit addInput(Node node)
+    {
+        this.inputs.add(node);
+        return this.addNode(node);
+    }
+
+    /**
+     * Adds a node to the list of probes, and to the list of nodes.
+     * @see #addNode
+     * @param node The node to add.
+     * @return The current circuit.
+     */
+    public Circuit addProbe(Node node)
+    {
+        this.probes.add(node);
+        return this.addNode(node);
+    }
+
     public void simulate()
     {
         // TODO:
