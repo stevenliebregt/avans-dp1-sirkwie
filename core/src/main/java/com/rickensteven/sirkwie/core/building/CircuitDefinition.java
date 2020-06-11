62fcbb65b639d77627ef6d1271edb60be1034426
thelegendtwentyseven <rick.berkers@gmail.com>
2020-06-11 09:20:13 +0200

    realled solved merge conflict

--- core/src/main/java/com/rickensteven/sirkwie/core/building/CircuitDefinition.java
+++ core/src/main/java/com/rickensteven/sirkwie/core/building/CircuitDefinition.java
@@ -1,10 +1,5 @@
 package com.rickensteven.sirkwie.core.building;
-<<<<<<< HEAD
 import java.util.ArrayList;
-import java.util.HashMap;
-=======
->>>>>>> 522f31a0468fedc4008323db3c70c7cf265a4443
-
 import java.util.HashMap;
 
 public class CircuitDefinition
@@ -13,7 +8,6 @@ public class CircuitDefinition
     private final HashMap<String, ArrayList<String>> edges;
 
     public CircuitDefinition(HashMap<String, String> nodes, HashMap<String, ArrayList<String>> edges) {
-    {
         this.nodes = nodes;
         this.edges = edges;
     }
